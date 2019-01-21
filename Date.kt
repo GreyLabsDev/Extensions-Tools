8afe9297e3e9df22ae7e2351045ad47b2b99cfdc
Sergey.Shardyko <greylabs@protonmail.com>
2019-01-21 18:27:14 +0500

    naming fix

--- Date.kt
+++ Date.kt
@@ -1,14 +1,14 @@
-fun Date.getCurrentTime() : String {
+fun Date.getCurrentTimeString() : String {
     val timeFormat = SimpleDateFormat("HH:mm", Locale.getDefault())
     return timeFormat.format(this.time)
 }
 
-fun Date.getCurrentWeekday() : String {
+fun Date.getCurrentWeekdayString() : String {
     val weekdayFormat = SimpleDateFormat("EEEE", Locale.getDefault())
     return weekdayFormat.format(this.time)
 }
 
-fun Date.getCurrentDate() : String {
+fun Date.getCurrentDateString() : String {
     val dateFormat = SimpleDateFormat("yyyy-MM-dd", Locale.getDefault())
     return dateFormat.format(this.time)
 }
