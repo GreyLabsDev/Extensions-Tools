886626a2eda559ede7c6b5a04ece90aae84c10cd
Sergey.Shardyko <greylabs@protonmail.com>
2019-03-19 17:44:10 +0500

    updated Date extensions

--- Date.kt
+++ Date.kt
@@ -3,6 +3,25 @@ fun Date.getCurrentTimeString() : String {
     return timeFormat.format(this.time)
 }
 
+fun Date.getCurrentTimeHour() : Int {
+    return this.getCurrentTimeString().split(":")[0].toInt()
+}
+
+fun Date.getCurrentTimeMinutes() : Int {
+    return this.getCurrentTimeString().split(":")[1].toInt()
+}
+
+fun Date.getMonthNumber(): Int {
+    val dateFormat = SimpleDateFormat("MM", Locale.getDefault())
+    val mStr = dateFormat.format(this.time)
+    return Integer.parseInt(mStr)
+}
+
+fun Date.getCurrentDayString(): String {
+    val dateFormat = SimpleDateFormat("dd", Locale.getDefault())
+    return dateFormat.format(this.time)
+}
+
 fun Date.getCurrentWeekdayString() : String {
     val weekdayFormat = SimpleDateFormat("EEEE", Locale.getDefault())
     return weekdayFormat.format(this.time)
