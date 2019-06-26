cbed3a19502b320a1ae592b690b56f05e486ece5
Sergey.Shardyko <greylabs@protonmail.com>
2019-06-26 19:12:32 +0300

    added view extensions

--- View.kt
+++ View.kt
@@ -15,3 +15,23 @@ fun View.animateFadeOut(duration: Long? = null,  onAnimationEndAction: (() -> Un
             .withEndAction { onAnimationEndAction?.invoke() }
             .start()
 }
+
+fun View.hide() {
+    this.visibility = View.GONE
+}
+
+fun View.show() {
+    this.visibility = View.VISIBLE
+}
+
+fun TextView.measureWidth(): Int {
+    val bound = Rect()
+    this.paint.getTextBounds(this.text.toString(), 0, this.text.toString().length, bound)
+    return bound.width()
+}
+
+fun TextView.measureHeight(): Int {
+    val bound = Rect()
+    this.paint.getTextBounds(this.text.toString(), 0, this.text.toString().length, bound)
+    return bound.height()
+}
