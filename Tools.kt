e0932567ef1f0aad949fae2d9da2536120ddae0f
Sergey.Shardyko <greylabs@protonmail.com>
2019-06-26 19:11:48 +0300

    added tools extensions

--- Tools.kt
+++ Tools.kt
@@ -1,3 +1,76 @@
 inline fun <T: Any, R: Any> multiLet(vararg variables: T?, out: (List<T>) -> R?): R? {
     return if (variables.all { it != null }) out (listOfNotNull(*variables)) else null
 }
+
+fun Double.metersToLatLonDegrees(): Double {
+    return this * 0.00001
+}
+
+fun <T>Collection<T>.hasOnlySingleItem(): Boolean {
+    return this.size == 1
+}
+
+fun <T>MutableList<T>.addOrRemove(element: T) {
+    if (this.contains(element)) this.remove(element) else this.add(element)
+}
+
+fun TextView.setTextOrHide(text: String?) {
+    text?.let {
+        this.text = it
+    } ?: run {
+        this.visibility = View.GONE
+    }
+}
+
+fun TextView.setTextWithAnimation(text: String, duration: Long) {
+    val stepDuration = duration/2
+    this.animate()
+            .alpha(0f)
+            .setDuration(stepDuration)
+            .withEndAction {
+                this.text = text
+                this.animate()
+                        .alpha(1f)
+                        .setDuration(stepDuration)
+                        .start()
+            }.start()
+}
+
+fun <T> Collection<T>?.joinToStringOrNull(separator: CharSequence): String? {
+    return when {
+        (this == null || this.count() == 0) -> null
+        else -> {
+            var result = ""
+            this.forEachIndexed {index, item ->
+                result += if (index != this.size - 1) "$item," else "$item"
+            }
+            result
+        }
+    }
+}
+
+fun <T> List<T>.second(): T? {
+    return when {
+        isEmpty() -> null
+        size < 2 -> null
+        else -> this[1]
+    }
+}
+
+fun <T>allIsEqual(vararg values: T): Boolean {
+    when {
+        values.isEmpty() -> return false
+        values.size == 1 -> return true
+    }
+    values.forEach {
+        if ((it == values.first()).not()) return false
+    }
+    return true
+}
+
+fun <T, R>allIsNotNull(vararg values: T, out: () -> R?): R? {
+    values.forEach {
+        if (it == null) return null
+    }
+    return out()
+}
