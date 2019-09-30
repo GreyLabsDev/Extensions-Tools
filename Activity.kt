96c66d8a649572a5c8c31fc16e5f8c2b75fc596a
Sergey.Shardyko <greylabs@protonmail.com>
2019-09-30 10:34:34 +0500

    added activity extensions
    great way to get extras from intent by lazy

--- Activity.kt
+++ Activity.kt
@@ -77,3 +77,13 @@ fun Activity.removeKeyboardListener(listener: ViewTreeObserver.OnGlobalLayoutLis
     val rootView = this.findViewById<View>(android.R.id.content)
     rootView.viewTreeObserver.removeOnGlobalLayoutListener(listener)
 }
+
+/**
+ * Getting serializable extras from intent by lazy. Define generic type for type casting
+ * 
+ * @property key - intent extra key
+ * 
+ * special thanks to Denis Performer (https://github.com/Perfomer)
+ */
+fun <T> Activity.argSerializable(key: String) = lazy { intent!!.getSerializableExtra(key)!! as T }
+fun <T> Activity.argSerializableNullable(key: String) = lazy { intent?.getSerializableExtra(key) as T }
