65d457ef9d7f8a9c35497b260b2195b999fae2f9
Sergey.Shardyko <greyson.dean@gmail.com>
2019-10-14 21:17:24 +0500

    rxtools updated

--- RxTools.kt
+++ RxTools.kt
@@ -3,10 +3,30 @@ fun <T: Any>Subject<T>.subscribeObserveDefault(): Observable<T> {
         .observeOn(AndroidSchedulers.mainThread())
 }
 
-fun <T: Any> Subject<T>.subscribeOnSchedulers(): Observable<T> {
+fun Completable.schedulersSubscribe(): Completable {
     return this.subscribeOn(Schedulers.io())
 }
 
-fun <T: Any>Observable<T>.observeOnMainThread(): Observable<T> {
+fun Completable.mainThreadObserve(): Completable {
+    return this.observeOn(AndroidSchedulers.mainThread())
+}
+
+fun <T: Any> Subject<T>.schedulersSubscribe(): Observable<T> {
+    return this.subscribeOn(Schedulers.io())
+}
+
+fun <T: Any> Observable<T>.schedulersSubscribe(): Observable<T> {
+    return this.subscribeOn(Schedulers.io())
+}
+
+fun <T: Any>Observable<T>.mainThreadObserve(): Observable<T> {
+    return this.observeOn(AndroidSchedulers.mainThread())
+}
+
+fun <T: Any> Single<T>.schedulersSubscribe(): Single<T> {
+    return this.subscribeOn(Schedulers.io())
+}
+
+fun <T: Any> Single<T>.mainThreadObserve(): Single<T> {
     return this.observeOn(AndroidSchedulers.mainThread())
 }
