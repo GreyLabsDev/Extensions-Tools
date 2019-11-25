a33719b011826b977fc24d6a8330aa3ab6d39b21
Sergey.Shardyko <greyson.dean@gmail.com>
2019-11-25 13:11:37 +0500

    added string extension
    Added string extension for email validation

--- Strings.kt
+++ Strings.kt
@@ -93,3 +93,16 @@ fun String.formatAsPrice(): String {
     formatter.applyPattern("#,###,###,### \u20BD")
     return "${formatter.format(this.toInt())}"
 }
+
+fun String.isValidEmail(): Boolean {
+    val emailRegex = compile(
+        "[a-zA-Z0-9\\+\\.\\_\\%\\-\\+]{1,256}" +
+                "\\@" +
+                "[a-zA-Z0-9][a-zA-Z0-9\\-]{0,64}" +
+                "(" +
+                "\\." +
+                "[a-zA-Z0-9][a-zA-Z0-9\\-]{0,25}" +
+                ")+"
+    )
+    return emailRegex.matcher(this).matches()
+}
